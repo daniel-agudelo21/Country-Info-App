9b59d3767f2b576f8e4abf560f9f46f24931b6c4
daniel-agudelo21 <daniel.agudelo185@gmail.com>
2024-09-30 20:21:06 -0500

    backend changes

--- server/src/controllers/countryController.js
+++ server/src/controllers/countryController.js
@@ -20,6 +20,7 @@ const getAllCountries = async (req, res) => {
 const getCountryById = async (req, res) => {
     const { id } = req.params;
     try {
+        
         const countryResponse = await axios(`${API_URL}/CountryInfo/${id}`);
         const country = countryResponse.data;
 
@@ -29,28 +30,38 @@ const getCountryById = async (req, res) => {
         const populationResponse = await axios(`${API_URL_2}/population`);
         const populationData = populationResponse.data.data;
 
-        const flag = flagData.find(f => f.iso2 === country.countryCode);
-        const population = populationData.find(p => p.iso3 === flag.iso3);
+        let flag = flagData.find(f => f.iso2 === country.countryCode);
 
-        if (flag && population) {
+        
+        let population = populationData.find(p => p.code.includes(country.countryCode));
+
+        
+        if (!population && flag) {
+            population = populationData.find(p => p.iso3 === flag.iso3);
+        }
+
+        
+        if (flag || population) {
             const countryData = {
                 id: country.countryCode,
-                flag: flag.flag,
+                flag: flag?.flag , 
                 name: country.commonName,
-                officialName: country.officialName,
-                region: country.region,
-                borders: country.borders,
-                population: population.populationCounts
+                officialName: country?.officialName || 'No official name available',
+                region: country?.region || 'No region available',
+                borders: country?.borders || [],
+                population: population?.populationCounts || 'No population data available',
             };
             res.status(200).json(countryData);
         } else {
-            res.status(404).json({ message: `Additional information does not found for the country with id: ${id}` });
+            
+            res.status(404).json({ message: `No information available for the country with id: ${id}` });
         }
     } catch (error) {
         res.status(400).json({ message: `Error searching for country with id: ${id}` });
         console.error(error);
     }
 };
+
 
 module.exports = {
     getAllCountries,
